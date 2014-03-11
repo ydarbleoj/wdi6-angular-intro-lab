ec5c022bc6d3a998d9054d5a18c8cb37199f6a3e
ydarbleoj <brady.joel@gmail.com>
2014-03-10 23:39:51 -0700

    add button is not working

--- app/books_controller.js
+++ app/books_controller.js
@@ -3,9 +3,40 @@ var bookly = bookly || {};
 bookly.bookApp = angular.module("bookApp", []);
 
 bookly.BooksController = function($scope) {
-  // load books. They are defined in data.js (we've got no back end yet!)
+  
   $scope.books = books;
+	$scope.allCart = [];
+ 	$scope.itemCount = 0;
 
-  //Code to manage cart goes here
+ 	// manage cart here
+ 	$scope.add = function(index){
+ 		var addedBook = $scope.books[index];
+
+ 		if($scope.checkout.indexOf(addedBook) >= 0) {
+ 			$scope.checkout[$scope.checkout.indexOf(addedBook)].quantity += 1;
+ 		} else {
+ 			addedBook.quantity = 1;
+ 			$scope.checkout.push(addedBook);
+ 		}
+ 		$scope.itemCount++;
+ 	};
+
+ 	$scope.total = function() {
+ 		var value = 0;
+
+ 		_.each($scope.checkout, function(element, index){
+ 			value += element.price * element.quantity;
+ 		});
+ 		if(value === 0){
+ 			return;
+ 		} else {
+ 			return money.toPrecision(4);
+ 		}
+ 	};
+	
+ 	$scope.empty = function(){
+ 		$scope.checkout = [];
+ 		$scope.itemCount = 0;
+ 	};
 
 };
